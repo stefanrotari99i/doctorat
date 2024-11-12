b49e5c2d6568e1a7c61402b06f32a040999637f4
unknown <stefan.rotari99i@gmail.com>
2024-11-12 18:41:57 +0200

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,79 @@
-import Image from "next/image";
+"use client";
+import { ComapreDate } from "@/components/CompareSensorDate";
+import { PieChartSensor } from "@/components/PieChart";
+import { Component } from "@/components/StatsSensor";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { type ChartConfig } from "@/components/ui/chart";
+import { Download, RefreshCcw, Text } from "lucide-react";
 
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex flex-col items-start p-10 gap-4 pb-20">
+      <h1 className="text-2xl font-semibold">Datele senzorului de deformare</h1>
+      <Card className="text-sm font-medium p-4 text-muted-foreground w-full">
+        ID senzor: <span className="text-primary">12did12sf3</span>
+        <br />
+        Data: <span className="text-primary">12/12/2022</span>
+        <br />
+        Ultima actualizare:{" "}
+        <span className="text-primary">12/12/2022 12:30</span>
+        <div>
+          Status activitate:{" "}
+          <span className="text-green-700 bg-green-500/20 rounded-lg text-xs py-1 px-4">
+            Inregistrare date
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+        <div className="flex-1">
+          <Component />
+        </div>
+        <div className="flex-1">
+          <ComapreDate />
+        </div>
+        <div className="flex-1">
+          <PieChartSensor />
+        </div>
+      </div>
+      <div className="flex fixed bottom-0 left-0 items-center justify-center md:justify-between px-6 bg-primary h-14 w-full">
+        <div className=" justify-center items-center hidden md:flex text-white text-sm font-medium">
+          Doctorat © 2025
+        </div>
+        <div className="flex items-center gap-4">
+          <Button className="rounded-xl" variant={"secondary"}>
+            <Download />
+            Exporta datele
+          </Button>
+          <Button className="rounded-xl" variant={"secondary"}>
+            <Text />
+            Rapoarte
+          </Button>
+          <Button className="rounded-xl" variant={"secondary"}>
+            <RefreshCcw />
+            Actualizeaza
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
